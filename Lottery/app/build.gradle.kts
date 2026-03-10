7352ca9e97d98991d5f7c00bc15a6d66794aa84c
zhA_zam <zphazam432@gmail.com>
2026-03-09 23:33:23 -0600

    feat: add back button to EntrantRegistrationActivity for navigation back to MainActivity

--- Lottery/app/build.gradle.kts
+++ Lottery/app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     // Import the Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:34.9.0"))
     implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-auth")
 
 }
 
--- Lottery/app/src/main/java/com/example/lottery/EntrantRegistrationActivity.java
+++ Lottery/app/src/main/java/com/example/lottery/EntrantRegistrationActivity.java
@@ -1,14 +1,24 @@
 package com.example.lottery;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class EntrantRegistrationActivity extends AppCompatActivity {
     private FirebaseFirestore db;
+    private EditText entrantName, entrantEmail, entrantPassword, entrantPassword2, entrantPhone;
+    private Button continueButton, anonContinueButton;
+    private ImageButton backButton;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +29,25 @@ public class EntrantRegistrationActivity extends AppCompatActivity {
 
         // Initialize firebase
         db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        // Initialize button views
+        backButton = findViewById(R.id.btnBack);
+        continueButton = findViewById(R.id.btnContinue);
+        anonContinueButton = findViewById(R.id.btnContinueWithoutRegistering);
+        // Initialize text editor views
+        entrantName = findViewById(R.id.etName);
+        entrantEmail = findViewById(R.id.etEmail);
+        entrantPhone = findViewById(R.id.etPhoneNumber);
+        entrantPassword = findViewById(R.id.etPassword);
+        entrantPassword2 = findViewById(R.id.etReEnterPassword);
+
+        // Set on click listeners for buttons
+        backButton.setOnClickListener(view -> {
+            Intent intent = new Intent(EntrantRegistrationActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+
     }
 }
